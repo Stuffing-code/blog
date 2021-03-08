ebdbb2700e9b7d9735bfac4a1627d2982e53d64f
Stuffcode <stuffingeu56@gmail.com>
2021-03-08 11:58:13 +0300

    create login check, and create users rights

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .forms import BlogForm
 from .models import BlogPost
@@ -13,6 +15,7 @@ def blog(request):
     context = {'texts': texts}
     return render(request, 'blogs/blog.html', context)
 
+@login_required
 def new_post(request):
     """Define new post"""
     if request.method != 'POST':
@@ -22,15 +25,19 @@ def new_post(request):
         # Date set POST
         form = BlogForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_post = form.save(commit=False)
+            new_post.owner = request.user
+            new_post.save()
             return redirect('blogs:blog')
 
     context = {'form': form}
     return render(request, 'blogs/new_post.html', context)
 
+@login_required
 def edit_post(request, text_id):
     text = BlogPost.objects.get(id=text_id)
     title = text.title
+    check_post_owner(request, text.owner)
     if request.method != 'POST':
         form = BlogForm(instance=text)
     else:
@@ -41,3 +48,9 @@ def edit_post(request, text_id):
 
     context = {'form': form, 'text': text, 'title': title}
     return render(request, 'blogs/edit_post.html', context)
+
+
+def check_post_owner(request, owner):
+    """Checking users owner or not"""
+    if owner != request.user:
+        raise Http404
