493098f4a522ffd2f9140d98a6b2ab97b176b0af
Andrey Yarovenko <stuffingeu56@gmail.com>
2021-03-13 07:36:03 +0300

    add css bootstrap for all page

--- blog/settings.py
+++ blog/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Others aplications
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
